7a85cbc14a155c441f341097f8b2084d091550a8
Azis <mikhsanazisp@gmail.com>
2025-06-20 22:20:17 +0700

    init countdownview

---
+++ vision-explore/Views/CountDownView.swift
@@ -0,0 +1,18 @@
+//
+//  CountDownView.swift
+//  vision-explore
+//
+//  Created by M Ikhsan Azis Pane on 20/06/25.
+//
+
+import SwiftUI
+
+struct CountDownView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CountDownView()
+}
