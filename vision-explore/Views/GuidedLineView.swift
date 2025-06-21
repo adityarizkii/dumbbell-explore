8f068af6d032deb05c351c94d20de5413d49cd15
Azis <mikhsanazisp@gmail.com>
2025-06-21 10:28:44 +0700

    Resolve merge conflict by deleting xcuserstate file

---
+++ vision-explore/Views/GuidedLineView.swift
@@ -0,0 +1,18 @@
+//
+//  GuidedLineView.swift
+//  vision-explore
+//
+//  Created by M Ikhsan Azis Pane on 21/06/25.
+//
+
+import SwiftUI
+
+struct GuidedLineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GuidedLineView()
+}
