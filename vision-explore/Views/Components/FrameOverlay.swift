3baa44e40c6ecfe63dc308d63b40e3fe10bcdada
Azis <mikhsanazisp@gmail.com>
2025-06-22 00:29:36 +0700

    fixing

--- vision-explore/Views/Components/FrameOverlay.swift
+++ vision-explore/Views/Components/FrameOverlay.swift
@@ -8,8 +8,59 @@
 import SwiftUI
 
 struct FrameOverlay: View {
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geo in
+            let frameHeight = geo.size.height * 0.8
+//            let frameHeight: CGFloat = 680
+            let cornerRadius: CGFloat = 15
+
+            ZStack {
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 50)
+                    .offset(y: 10)
+                    .frame(maxWidth: .infinity, maxHeight: frameHeight)
+                    .blendMode(.destinationOut)
+
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .trim(from: 0.125, to: 0.375)
+                    .stroke(
+                        LinearGradient(
+                            gradient: Gradient(colors: [Color("Button1"),Color("Button2") ]),
+                            startPoint: .trailing,
+                            endPoint: .leading
+                            
+                        ),
+                        lineWidth: 4
+                    )
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 50)
+                    .offset(y: 10)
+                    .frame(maxWidth: .infinity, maxHeight: frameHeight)
+
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .trim(from: 0.625, to: 0.875)
+                    .stroke(
+                       LinearGradient(
+                            gradient: Gradient(colors: [Color("Button1"),Color("Button2") ]),
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        ),
+                        lineWidth: 4
+                    )
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 50)
+                    .offset(y: 10)
+                    .frame(maxWidth: .infinity, maxHeight: frameHeight)
+            }
+            .frame(width: geo.size.width, height: geo.size.height)
+            .compositingGroup()
+            .position(x: geo.size.width / 2, y: geo.size.height / 2) 
+        }
+        .ignoresSafeArea()
+
     }
 }
 
