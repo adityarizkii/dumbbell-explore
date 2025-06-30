da190cc377340794545dedc05f9a3f52e20e0e0a
MChandraR <chandramadhan04@gmail.com>
2025-06-30 14:38:32 +0700

    Resolve watch bundle not integrated to the app

--- BumdleWatch Watch App/BumdleWatchApp.swift
+++ BumdleWatch Watch App/BumdleWatchApp.swift
@@ -2,24 +2,16 @@
 //  BumdleWatchApp.swift
 //  BumdleWatch Watch App
 //
-//  Created by Muhammad Chandra Ramadhan on 28/06/25.
+//  Created by Muhammad Chandra Ramadhan on 30/06/25.
 //
 
 import SwiftUI
-import Foundation
-import WatchConnectivity
-import WatchKit
 
 @main
 struct BumdleWatch_Watch_AppApp: App {
-    @WKExtensionDelegateAdaptor(WatchSessionManager.self) var delegate
-
     var body: some Scene {
         WindowGroup {
-            ContentView(manager : delegate)
+            ContentView()
         }
     }
 }
-
-
-
--- BumdleWatch Watch App/ContentView.swift
+++ BumdleWatch Watch App/ContentView.swift
@@ -2,37 +2,23 @@
 //  ContentView.swift
 //  BumdleWatch Watch App
 //
-//  Created by Muhammad Chandra Ramadhan on 28/06/25.
+//  Created by Muhammad Chandra Ramadhan on 30/06/25.
 //
 
 import SwiftUI
-import WatchConnectivity
-struct ContentView: View {
-    @ObservedObject var manager: WatchSessionManager
 
+struct ContentView: View {
     var body: some View {
-        if manager.exerciseDetail.exercise == "" {
-            VStack {
-                Spacer()
-                Image("Icon")
-                    .resizable()
-                    .frame(width: 50, height:50)
-                
-                    .cornerRadius(25)
-                
-                Spacer()
-                Text(manager.text)
-                Spacer()
-            }
-            .padding()
-        }else{
-            ExerciseSession(manager: manager)
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
-        
+        .padding()
     }
 }
 
-
 #Preview {
-    ContentView(manager : WatchSessionManager() )
+    ContentView()
 }
